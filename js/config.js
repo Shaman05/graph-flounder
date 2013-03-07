ed3f0799bccf82abc4298acdf193a78365fd3b65
shaman <27231583@qq.com>
2013-03-07 17:42:15 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Devin Chen
+ * Date: 3/7/13
+ * Time: 3:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+;seajs.config({
+    base: './js/module/',
+    debug: true,
+    charset: 'utf-8'
+});
---
+++ js/module/DHTMLGraph.js
@@ -0,0 +1,113 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Devin Chen
+ * Date: 3/7/13
+ * Time: 2:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+;define(function(require, exports, module){
+    'use strict';
+
+    /**
+     * 私有方法：创建一个 sprite 对象
+     * @param param
+     * @return {Object}
+     * @constructor
+     */
+    var MySprite = function(param){
+        var $box = param.box;
+        var h = param.height;
+        var w = param.width;
+        var url = param.imgUrl;
+        var imgW = param.imgWidth;
+        var defaultStyle = {
+            'height': h,
+            'width': w,
+            'position': 'absolute',
+            'backgroundImage': 'url(' + url + ')'
+        };
+        var $elem = $('<div></div>', {css: defaultStyle}).appendTo($box);
+        var elemStyle = $elem[0].style;
+        var mathFloor = Math.floor;
+        return  {
+            setPosition: function(x, y){
+                elemStyle.left = x + 'px';
+                elemStyle.top = y + 'px';
+            },
+            changeImg: function(index){
+                index *= w;
+                var x = - index % imgW;
+                var y = - mathFloor(index/imgW) * h;
+                elemStyle.backgroundPosition = x + 'px ' + y + 'px';
+            },
+            show: function(){
+                elemStyle.display = 'block';
+            },
+            hide: function(){
+                elemStyle.display = 'none';
+            },
+            toggle: function(){
+                $elem.fadeToggle(500);
+            },
+            remove: function(){
+                $elem.remove();
+            }
+        };
+    };
+
+    module.exports = {
+        /**
+         * 定义一个过渡动画, 如果是静止的, 则无需调用 move 方法
+         * @param param 公用参数
+         * @param extendParam 扩展参数
+         * @return {*} 动画对象
+         */
+        CreateElem: function(param, extendParam){
+            param.height = extendParam.size;
+            param.width = extendParam.size;
+            var index = extendParam.indexRange[0] - 1;
+            var startIndex = extendParam.indexRange[0] - 1;
+            var endIndex = extendParam.indexRange[1] - 1;
+            var that = MySprite(param);
+            var x = extendParam.x;
+            var y = extendParam.y;
+            var xDir = extendParam.xDir;
+            var yDir = extendParam.yDir;
+            var maxX = param.maxX;
+            var maxY = param.maxY;
+            that.setPosition(x, y);
+            that.move = function(isCrash){
+                index ++;
+                index = index > endIndex ? startIndex : index;
+                that.changeImg(index);
+                if(isCrash){
+                    x += xDir;
+                    y += yDir;
+                    if((xDir < 0 && x < 0) || (xDir > 0 && x >= maxX)){
+                        xDir = -xDir;
+                    }
+                    if((yDir < 0 && y < 0) || (yDir > 0 && y >= maxY)){
+                        yDir = -yDir;
+                    }
+                    that.setPosition(x, y);
+                }
+            };
+            return that;
+        },
+
+        /**
+         * 分组动画方法
+         * @param objArr 动画对象集合
+         * @param isCrash 是否碰撞
+         * @constructor
+         */
+        Animate: function(objArr, isCrash){
+            var _isCrash = isCrash || false;
+            var len = objArr.length;
+            while(len --){
+                objArr[len].move(_isCrash);
+            }
+        }
+    }
+});
