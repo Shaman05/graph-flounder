8399cf575395f1d2b3348fbffbc062a72d1d6956
shaman <27231583@qq.com>
2013-03-15 15:17:04 +0800

    create

---
+++ chinesechess/public/javascripts/chinesechess.js
@@ -0,0 +1,52 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Shaman
+ * Date: 13-3-12
+ * Time: 下午9:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+;(function(){
+
+    var panel = $('panel');
+    var viewer = $('viewer');
+    var red = $('red');
+    var black = $('black');
+
+    seajs.config({
+        base: './javascripts/module/',
+        debug: true
+    });
+
+    seajs.use(['chessboard','chess','data'], function(board, chess, data){
+        board.init();
+
+        $('gameType').selectedIndex = 0;
+        viewer.disabled = false;
+        viewer.checked = true;
+        red.disabled = false;
+        black.disabled = false;
+
+        panel.onclick = function(e){
+            var evt = e || window.event;
+            var _this = evt.target;
+            if(_this.getAttribute('type') == 'radio'){
+                composition(_this.value);
+            }
+        };
+
+        function composition(camp){
+            viewer.disabled = true;
+            red.disabled = true;
+            black.disabled = true;
+            data.player.type = camp;
+            data.player.canMove = (camp == 'red');
+            board.createChess(camp, chess);
+        }
+    });
+
+    function $(id){
+        return document.getElementById(id);
+    }
+
+})();
---
+++ chinesechess/public/javascripts/module/validation.js
@@ -0,0 +1,45 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: chenD1
+ * Date: 3/14/13
+ * Time: 2:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+;define(function(require, exports, module){
+
+    module.exports = {
+
+        //检测是否能走棋到目标位置
+        canMoveTo: function(obj, target){
+
+        },
+
+        //检测两个位置中间是否有棋子，只有中间有且仅有一颗棋子的时候才返回真值
+        hasChessInMiddle: function(obj, target){
+
+        },
+
+        //检测兵（卒）是否过河
+        isSuperMan: function(){
+
+        },
+
+        //检测是否撇马脚
+        isHorseBlocked: function(){
+
+        },
+
+        //检测相（象）心是否被堵
+        isElephantBlocked: function(){
+
+        },
+
+        //检测将和相是否碰面
+        isFaceToFace: function(){
+
+        }
+
+    }
+
+});
